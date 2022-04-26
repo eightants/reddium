bd6e600014bcc77718f4590f78a94db5cf6fce00
Anthony <yihonganthony@gmail.com>
2022-04-26 00:27:54 -0500

    Added ga4 proper

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import { GA_TRACKING_ID } from "../functions/gtag";
+import { GA4_ID, GA_TRACKING_ID } from "../functions/gtag";
 
 export default class MyDocument extends Document {
   render() {
@@ -13,12 +13,19 @@ export default class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`}
+          />
+          <script
             dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+              gtag('config', '${GA4_ID}', {
                 page_path: window.location.pathname,
               });
           `
